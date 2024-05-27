4acda0f77b6eae28660930cf765664b75bd550e4
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-05-27 14:32:19 +0800

    Update auth.py

--- tcplib/auth.py
+++ tcplib/auth.py
@@ -1,4 +1,10 @@
 from .create import s , createServer
 
-def auth_user(host : str,port : int,username : str, password : str):
-  testSer = createServer(hostname=s.gethostname(),port="5090",authUser=username,authPassword=password)
+def auth_login(host : str,port : int,username : str, password : str):
+  #testSer = createServer(hostname=host,port=port,authUser=username,authPassword=password)
+  s.connect((host,port))
+  userFormat = f"AUTH-USER {username}"
+  pwFormat = f"AUTH-PW {password}"
+  s.send(userFormat.encode("utf-8"))
+  s.send(userFormat.encode("utf-8"))
+  return s.recv(1024)
