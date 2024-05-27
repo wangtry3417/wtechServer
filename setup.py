db509051efb36df2775eba47dffc6f750b5904a8
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-05-27 14:06:51 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tcplib',
+    version='0.1',
+    description='That a tcp maker.',
+    author='WTech',
+    author_email='wangtry3417@gmail.com',
+    packages=find_packages(),
+    license='MLT',
+    license_files=('LICENSE'),
+    install_requires=[
+        'cryptography',
+        'requests',
+        'random2',
+        'discord'
+    ],
+)
